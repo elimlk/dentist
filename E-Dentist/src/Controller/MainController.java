b1545366444ad75a6ca2122c05a5254a6b13b004
liorkarichly <liorkricheli1@gamil.com>
2020-07-05 00:31:34 +0300

    mainController fiixed by gabi

--- E-Dentist/src/Controller/MainController.java
+++ E-Dentist/src/Controller/MainController.java
@@ -25,20 +25,51 @@ public class MainController {
         }
     }
 
-    public boolean register(String id, String firstName, String lastName, String phone,String email, String password, boolean instructor) {
+    public boolean register(String id, String firstName, String lastName, String phone, String email, String password, boolean instructor) {
         //valditaion test
-        int id_int = 0;
-        if (!(id.length()==9 || id.length()==8))
-            return false;
+        
+    	int id_int = 0;
+        boolean worangInput = false;
+        
+        if (id.length() != 9 || id.length() != 8)
+        	
+            return worangInput;
+      
         try {
+        	
             id_int = Integer.parseInt(id);
+            
         } catch (NumberFormatException e) {
-            return false;
+            
+        	return worangInput;
             //e.printStackTrace();
         }
+      //complete validation tests
+        if(!(phone.length() == 10))  //phone val
+         
+        	return worangInput;
+        
+        try {
+        	
+            int_replace = Integer.parseInt(phone);
+            
+        } catch (NumberFormatException e) {
+           
+        	return worangInput;
+            //e.printStackTrace();
+        }
+        //email val//
+        if(!(email.contains("@") || email.contains(".co.il") ||  email.contains(".com")  || email.contains(".ac.il")))
+		
+        	return worangInput;
+        
+	 if(email.contains("@@"))
+		 
+		return worangInput;
+
         //complete validation tests
 
         mainModel.register(id,firstName,lastName,phone,email,password,instructor);
-        return true;
+        return !worangInput;
     }
 }
