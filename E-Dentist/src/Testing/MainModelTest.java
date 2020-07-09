25d8c95bde32427caeab47320495c93bfaff583c
Itamar <ithamar000@yahoo.com>
2020-07-10 00:44:17 +0300

    Testing the tests

---
+++ E-Dentist/src/Testing/MainModelTest.java
@@ -0,0 +1,20 @@
+package Testing;
+
+import Controller.MainController;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainModelTest {
+
+    @Test
+    void register() throws IOException {
+        MainController mainController = new MainController();
+        boolean regValue = mainController.register("123123123","Itamar","Yacoby","0545934997","ithamar000@gmail.com","1234",false);
+        assertTrue(regValue);
+        regValue = mainController.register("12123","Itamar","Yacoby","0545934997","ithamar000@gmail.com","1234",false);
+        assertFalse(regValue);
+    }
+}
